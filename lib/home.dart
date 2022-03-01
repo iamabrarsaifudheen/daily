9794fb67f1c4379730cb2092708926f145f1a252
iamabrarsaifudheen <72561669+iamabrarsaifudheen@users.noreply.github.com>
2022-03-01 16:34:31 +0530

    Added Authentication

---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:daily/application_state.dart';
+import 'package:daily/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'authentication.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Consumer<ApplicationState>(
+            builder: (context, appState, _) => Authentication(
+              email: appState.email,
+              loginState: appState.loginState,
+              startLoginFlow: appState.startLoginFlow,
+              verifyEmail: appState.verifyEmail,
+              signInWithEmailAndPassword: appState.signInWithEmailAndPassword,
+              cancelRegistration: appState.cancelRegistration,
+              registerAccount: appState.registerAccount,
+              signOut: appState.signOut,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'package:daily/sign_in.dart';
+
+import 'package:daily/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'application_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(const MyApp());
+  runApp( ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, _) => const MyApp(),
+    ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +29,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: const Color(0xffFFFAF6),
         primaryColor: const Color(0xffF5B9A7),
       ),
-      home: const SignIn(),
+      home: const Home(),
     );
   }
 }
